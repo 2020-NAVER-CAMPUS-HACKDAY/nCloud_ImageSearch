c49a32f00aa6e70535b0a67b1b645a6624fb3e73
heojungeun <heocube@naver.com>
2020-05-14 22:44:02 +0900

    feat-#5[photoviewr]viewer loadImage 사진 화면 출력

--- app/src/main/java/com/hackday/imageSearch/ui/viewer/ViewerActivity.kt
+++ app/src/main/java/com/hackday/imageSearch/ui/viewer/ViewerActivity.kt
@@ -2,12 +2,20 @@ package com.hackday.imageSearch.ui.viewer
 
 import android.net.Uri
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.hackday.imageSearch.R
 import com.hackday.imageSearch._base.BaseActivity
 import com.hackday.imageSearch.databinding.ActivityViewerBinding
+import kotlinx.android.synthetic.main.activity_viewer.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ViewerActivity : BaseActivity<ActivityViewerBinding>() {
+
+    private lateinit var id: String
+    private lateinit var uri: String
+
     override val vm: ViewerViewModel by viewModel()
     override fun getLayoutRes() = R.layout.activity_viewer
 
@@ -17,10 +25,42 @@ class ViewerActivity : BaseActivity<ActivityViewerBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        loadImage()
     }
 
-    fun updateInfo(id: String, uri: String){
-        // id와 uri 를 통해 db에서 사진의 디테일 정보 불러오기
+    override fun onStart() {
+        super.onStart()
+
+        btn_back.setOnClickListener {
+            finish()
+        }
+
+        btn_detail.setOnClickListener {
+            loadDetail()
+        }
+    }
+
+    fun loadImage(){
+        // photoFragment 에서 전달되는 id, uri 받기
+        if(intent != null && intent.hasExtra(EXTRA_PHOTO_ID) && intent.hasExtra(EXTRA_PHOTO_URI)){
+            id = intent.getStringExtra(EXTRA_PHOTO_ID)!!
+            uri = intent.getStringExtra(EXTRA_PHOTO_URI)!!
+
+            Glide.with(this)
+                .load(uri)
+                .error(R.drawable.ic_launcher_background)
+                .apply(RequestOptions().fitCenter())
+                .into(img_photo_detail)
+        }
+    }
+
+    fun loadDetail(){
+        // uri를 통해 photo 데이터 로드
+        var res = vm.getPhotoByUri(uri)
+
+        // dialog에 출력
+
     }
 
     companion object{
--- app/src/main/java/com/hackday/imageSearch/ui/viewer/ViewerViewModel.kt
+++ app/src/main/java/com/hackday/imageSearch/ui/viewer/ViewerViewModel.kt
@@ -1,14 +1,20 @@
 package com.hackday.imageSearch.ui.viewer
 
+import android.content.Intent
 import androidx.lifecycle.ViewModel
+import com.hackday.imageSearch.repository.PhotoInfoRepositoryImpl
 
 class ViewerViewModel : ViewModel() {
 
-    fun onClickback(){
-        // back btn click
-    }
+    private var repository: PhotoInfoRepositoryImpl = PhotoInfoRepositoryImpl()
+    var id_ : String = ""
+    var uri_ : String = ""
 
-    fun onClickInfo(){
-        // photo info detail click
-    }
+    fun getPhotoByUri(uri: String) = repository.getPhotoByUri(uri)
+
+    fun getPhotoById(id: String) = repository.getPhotoById(id)
+
+//    fun onClickInfo(){
+//
+//    }
 }

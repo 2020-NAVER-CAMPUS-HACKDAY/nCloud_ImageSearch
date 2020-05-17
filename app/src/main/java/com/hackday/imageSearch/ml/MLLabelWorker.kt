a92f7c5bac5db15a16ab27fd81ca89a38e079a11
ryujisuda <cherryhee_@naver.com>
2020-05-17 22:53:45 +0900

    feat-#7 [ml] 핵심 기능 구현

--- app/src/main/java/com/hackday/imageSearch/ml/MLLabelWorker.kt
+++ app/src/main/java/com/hackday/imageSearch/ml/MLLabelWorker.kt
@@ -1,21 +1,33 @@
 package com.hackday.imageSearch.ml
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.room.Room
 import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import com.hackday.imageSearch.database.PhotoInfoDatabase
+import java.io.IOException
 
 
 class MLLabelWorker (private val context: Context, private val workerParams:WorkerParameters) : Worker(context,workerParams){
 
-    private var pathArrayList = ArrayList<String>()
+    private var pathArrayList = ArrayList<Uri>()
+
+
     override fun doWork(): Result {
         try {
+            getPermission()
             addLabelToImageIfNeeded {path, current ->
                 reportProgress(current)
+
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -58,9 +70,27 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
 
     private fun addLabelToImageIfNeeded(doThis: (String,  Int) -> Any ) {
 
+        getPermission()
+        lateinit var labelImage : FirebaseVisionImage
         for(i in pathArrayList)
         {
-            //TODO : MLkit로 라벨링 하면서 DB에 삽입하기
+            try {
+                //TODO : URI를 이용해 firepath를 찾아야하는데..
+                labelImage = FirebaseVisionImage.fromFilePath(context,pathArrayList[i])
+            }catch (e:IOException)
+            {
+                e.printStackTrace()
+            }
+
+            val detector = FirebaseVision.getInstance().getCloudImageLabeler()
+            detector.processImage(labelImage)
+                .addOnSuccessListener { labels->
+                    //TODO DB에 삽입
+
+                }
+
+
+
         }
     }
 
@@ -68,5 +98,23 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
         // setProgressAsync() -> MLActivity에 observe에 걸린다.
         val newData = Data.Builder().putInt("progress",progress).build()
         setProgressAsync(newData)
+    }
+
+    private fun getPermission()
+    {
+        var permissionListener: PermissionListener = object:PermissionListener {
+
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+            }
+        }
+
+        TedPermission.with(applicationContext) .setPermissionListener(permissionListener)
+            .setDeniedMessage("권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용해주세요.")
+            .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE) .check();
     }
 }

9da2f10a220bf19e8d02d143dfe36301036be8f2
ryujisuda <cherryhee_@naver.com>
2020-05-18 19:24:39 +0900

    feat-#7 [ml] 핵심 기능 구현

--- app/src/main/java/com/hackday/imageSearch/ml/MLLabelWorker.kt
+++ app/src/main/java/com/hackday/imageSearch/ml/MLLabelWorker.kt
@@ -3,11 +3,17 @@ package com.hackday.imageSearch.ml
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentUris
 import android.content.Context
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationBuilderWithBuilderAccessor
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.room.Room
 import androidx.work.*
 import com.google.firebase.ml.vision.FirebaseVision
@@ -15,13 +21,14 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import com.hackday.imageSearch.database.PhotoInfoDatabase
+import com.hackday.imageSearch.model.PhotoInfo
 import java.io.IOException
 import java.net.URI
 
 
 class MLLabelWorker (private val context: Context, private val workerParams:WorkerParameters) : Worker(context,workerParams){
 
-    private var pathArrayList = ArrayList<String>()
+    private var pathArrayList = ArrayList< Pair<String,String> >()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun doWork(): Result {
@@ -41,6 +48,14 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createForegroundInfo(progress :String):ForegroundInfo{
+
+        val channelId = if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+        {
+            createNotificationChannel("channelId","channelName")
+        }else
+        {
+
+        }
         val notification = Notification.Builder(applicationContext,"${context.packageName}")
             .setContentText(progress)
             .build()
@@ -48,6 +63,17 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
         return ForegroundInfo(1,notification)
 
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String):String {
+        val chan = NotificationChannel(channelId,channelName, NotificationManager.IMPORTANCE_NONE)
+
+        val service :NotificationManager = getSystemService(context,NotificationManager::class.java) as NotificationManager
+        service.createNotificationChannel(chan)
+
+        return channelId
+    }
+
     private fun getNoneLabeledList()
     {
         val idColumnName = MediaStore.Images.ImageColumns._ID
@@ -56,20 +82,24 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
         val uri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val projection = arrayOf(idColumnName, pathColumnName)
 
-        val cursor = context
+        context
             .contentResolver
             .query(uri, projection, null, null, MediaStore.MediaColumns.DATE_ADDED + " desc")
-            ?: throw RuntimeException("This should not happen!")
+            ?.use {
+                val idColumnIndex = it.getColumnIndexOrThrow(idColumnName)
 
-        val idColumnIndex = cursor.getColumnIndexOrThrow(idColumnName)
-        val pathColumnIndex: Int = cursor.getColumnIndexOrThrow(pathColumnName)
+                while (it.moveToNext()) {
+                    val uriAndDate = Pair(ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        it.getLong(idColumnIndex)).toString(),MediaStore.Images.Media.DATE_ADDED.toString())
+                    pathArrayList.add(
+                        uriAndDate
+                    )
+                }
+            }
 
-        while (cursor.moveToNext()) {
-            val path = cursor.getString(pathColumnIndex)
-            pathArrayList.add(path)
-        }
 
-        cursor.close();
+
     }
 
 
@@ -77,21 +107,40 @@ class MLLabelWorker (private val context: Context, private val workerParams:Work
 
         var howManyLabeled = 0
         lateinit var labelImage : FirebaseVisionImage
-        for(uri in pathArrayList)
+        for(uriAndDate in pathArrayList)
         {
             try {
-                labelImage = FirebaseVisionImage.fromFilePath(context,Uri.parse(uri))
+                labelImage = FirebaseVisionImage.fromFilePath(context,Uri.parse(uriAndDate.first.toString()))
             }catch (e:IOException)
             {
                 e.printStackTrace()
             }
 
-            val detector = FirebaseVision.getInstance().getCloudImageLabeler()
+            val detector = FirebaseVision.getInstance().getOnDeviceImageLabeler()
             detector.processImage(labelImage)
                 .addOnSuccessListener { labels->
-                    //TODO : DB에 삽입
-                    doThis(++howManyLabeled,pathArrayList.size)
+                    if(labels.size==0)
+                    {
+                        PhotoInfoDatabase.getInstance().photoInfoDao().insertPhoto(PhotoInfo(uriAndDate.first,uriAndDate.second,
+                            null,null,null));
+                    }
+                    else if(labels.size==1)
+                    {
+                        PhotoInfoDatabase.getInstance().photoInfoDao().insertPhoto(PhotoInfo(uriAndDate.first,uriAndDate.second,
+                            labels[0].text,null,null));
+                    }
+                    else if(labels.size==2)
+                    {
+                        PhotoInfoDatabase.getInstance().photoInfoDao().insertPhoto(PhotoInfo(uriAndDate.first,uriAndDate.second,
+                            labels[0].text,labels[1].text,null));
+                    }
+                    else
+                    {
+                        PhotoInfoDatabase.getInstance().photoInfoDao().insertPhoto(PhotoInfo(uriAndDate.first,uriAndDate.second,
+                            labels[0].text,labels[1].text,labels[2].text));
+                    }
 
+                    doThis(++howManyLabeled,pathArrayList.size)
                 }
         }
     }

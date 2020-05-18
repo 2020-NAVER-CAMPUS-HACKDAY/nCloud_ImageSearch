ad0d6b1367fd70f55654d793b889b7cb9a4fe7c4
YChaeeun <ychae.leah@gmail.com>
2020-05-19 04:38:04 +0900

    feat/main-paging adapter, fragment, viewModel 수정

--- app/src/main/java/com/hackday/imageSearch/ui/photo/PhotoFragment.kt
+++ app/src/main/java/com/hackday/imageSearch/ui/photo/PhotoFragment.kt
@@ -2,18 +2,19 @@ package com.hackday.imageSearch.ui.photo
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hackday.imageSearch.R
 import com.hackday.imageSearch._base.BaseFragment
 import com.hackday.imageSearch.databinding.FragmentPhotoBinding
 import com.hackday.imageSearch.model.PhotoInfo
+import com.hackday.imageSearch.ui.photo.adapter.PhotoAdapter
 import com.hackday.imageSearch.ui.photo.adapter.PhotoItemDecorator
-import com.hackday.imageSearch.ui.photo.adapter.PhotoRecyclerAdapter
 import com.hackday.imageSearch.ui.viewer.ViewerActivity
 import kotlinx.android.synthetic.main.fragment_album.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class PhotoFragment : BaseFragment<FragmentPhotoBinding>(), photoClickListener {
+class PhotoFragment : BaseFragment<FragmentPhotoBinding>(), PhotoClickListener {
     override val vm: PhotoViewModel by viewModel()
     override fun getLayoutRes() = R.layout.fragment_photo
     override fun setupBinding() {
@@ -22,27 +23,31 @@ class PhotoFragment : BaseFragment<FragmentPhotoBinding>(), photoClickListener {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        vm.getAll()
+
         setRecyclerView()
     }
 
     private fun setRecyclerView() {
 
         val recyclerManager = GridLayoutManager(context, 4)
+        val photoAdapter = PhotoAdapter { photo ->
+            photoClicked(photo)
+        }
+        vm.itemList.observe(viewLifecycleOwner, Observer {
+            photoAdapter.submitList(it)
+        })
 
         with(layout_recycler_view) {
-            adapter = PhotoRecyclerAdapter { photo ->
-                photoClicked(photo)
-            }
+            adapter = photoAdapter
             layoutManager = recyclerManager
             setHasFixedSize(false)
             addItemDecoration(PhotoItemDecorator())
         }
     }
 
-    override fun photoClicked(photo: PhotoInfo) {
+    override fun photoClicked(photo: PhotoInfo?) {
         val intent = Intent(context, ViewerActivity::class.java)
-        intent.putExtra(ViewerActivity.EXTRA_PHOTO_URI, photo.uri)
+        intent.putExtra(ViewerActivity.EXTRA_PHOTO_URI, photo?.uri)
         startActivity(intent)
     }
 

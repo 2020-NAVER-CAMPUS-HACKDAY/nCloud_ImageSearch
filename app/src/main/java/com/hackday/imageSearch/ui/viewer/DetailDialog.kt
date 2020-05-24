7ec8f754942dab4c95d49aa50dbc04084b7adef8
heojungeun <heocube@naver.com>
2020-05-24 19:11:47 +0900

    feat-#5[photoviewr] 테스트용 버튼 삭제, 뷰어 버튼 위로 보이게 하기

--- app/src/main/java/com/hackday/imageSearch/ui/viewer/DetailDialog.kt
+++ app/src/main/java/com/hackday/imageSearch/ui/viewer/DetailDialog.kt
@@ -12,7 +12,7 @@ class DetailDialog (context : Context) {
     private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
 
     fun start(photo : PhotoInfo) {
-        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
+        //dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(R.layout.dialog_viewer_infodetail)     //다이얼로그에 사용할 xml 파일을 불러옴
         dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
